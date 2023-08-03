5cbd53d4c220a64196de0fa305cd193a526076d6
max <ljk8540009@imcloud.co.kr>
2023-08-03 10:12:38 +0900

    feat: kafka 통신 중, Jackson 추가하여java.time.LocalDateTime 문제 해결

--- src/main/java/com/imcloud/saas_user/kafka/service/UserEventProducer.java
+++ src/main/java/com/imcloud/saas_user/kafka/service/UserEventProducer.java
@@ -2,6 +2,7 @@ package com.imcloud.saas_user.kafka.service;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.imcloud.saas_user.common.security.UserDetailsImpl;
 
 import org.springframework.kafka.core.KafkaTemplate;
@@ -12,13 +13,14 @@ import org.springframework.beans.factory.annotation.Value;
 public class UserEventProducer {
 
     private final KafkaTemplate<String, String> kafkaTemplate;
-    private final ObjectMapper objectMapper = new ObjectMapper();
+    private final ObjectMapper objectMapper;
 
     private final String user_to_pay_topic = "user_to_pay_topic";
     private final String user_to_admin_topic = "user_to_admin_topic";
 
     public UserEventProducer(KafkaTemplate<String, String> kafkaTemplate) {
         this.kafkaTemplate = kafkaTemplate;
+        this.objectMapper = new ObjectMapper().registerModule(new JavaTimeModule()); // Register JavaTimeModule
     }
 
     public void sendUserDetails(UserDetailsImpl userDetails) {
