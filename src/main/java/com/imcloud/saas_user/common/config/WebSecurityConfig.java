349579c25d86b28314e97a4022f1a1604edeecf8
max <ljk8540009@imcloud.co.kr>
2023-08-25 23:12:05 +0900

    feat: allowedOrigin allow all

--- src/main/java/com/imcloud/saas_user/common/config/WebSecurityConfig.java
+++ src/main/java/com/imcloud/saas_user/common/config/WebSecurityConfig.java
@@ -82,24 +82,35 @@ public class WebSecurityConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        // TODO: 배포시, 아래 코드 수정 절대적으로 할 것
-        ArrayList<String> allowedOriginList = new ArrayList<>();
-        for (int port = 3000; port <= 3010; ++port) {
-            allowedOriginList.add("http://localhost:"+ port);
-        }
-        allowedOriginList.add("https://open-velog.vercel.app");
-        allowedOriginList.add("https://search-open-velog-elastic-bvtge6dkgdmsr2i3f3kzwo6m2u.ap-northeast-2.es.amazonaws.com");
-        allowedOriginList.add("http://43.131.252.119:8099/");
-        allowedOriginList.add("http://localhost:8800/");
-
-        registry
-                .addMapping("/**") // 프로그램에서 제공하는 URL
-                .allowedOrigins(allowedOriginList.toArray(new String[0])) // 요청을 허용할 출처를 명시, 전체 허용 (가능하다면 목록을 작성한다.
-                .allowedMethods("GET", "POST", "OPTIONS", "PUT", "DELETE") // 어떤 메서드를 허용할 것인지 (GET, POST...)
-                .allowedHeaders("Content-Type", "Authorization") // 어떤 헤더들을 허용할 것인지
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "OPTIONS", "PUT", "DELETE")
+                .allowedHeaders("Content-Type", "Authorization")
                 .exposedHeaders("Content-Type", "Authorization")
-                .allowCredentials(true) // 쿠키 요청을 허용한다(다른 도메인 서버에 인증하는 경우에만 사용해야하며, true 설정시 보안상 이슈가 발생할 수 있다)
-                .maxAge(216000); // preflight 요청에 대한 응답을 브라우저에서 캐싱하는 시간;
+                .allowCredentials(true)
+                .maxAge(216000);
     }
+
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        // TODO: 배포시, 아래 코드 수정 절대적으로 할 것
+//        ArrayList<String> allowedOriginList = new ArrayList<>();
+//        for (int port = 3000; port <= 3010; ++port) {
+//            allowedOriginList.add("http://localhost:"+ port);
+//        }
+//        allowedOriginList.add("https://open-velog.vercel.app");
+//        allowedOriginList.add("https://search-open-velog-elastic-bvtge6dkgdmsr2i3f3kzwo6m2u.ap-northeast-2.es.amazonaws.com");
+//        allowedOriginList.add("http://43.131.252.119:8099/");
+//        allowedOriginList.add("http://localhost:8800/");
+//
+//        registry
+//                .addMapping("/**") // 프로그램에서 제공하는 URL
+//                .allowedOrigins(allowedOriginList.toArray(new String[0])) // 요청을 허용할 출처를 명시, 전체 허용 (가능하다면 목록을 작성한다.
+//                .allowedMethods("GET", "POST", "OPTIONS", "PUT", "DELETE") // 어떤 메서드를 허용할 것인지 (GET, POST...)
+//                .allowedHeaders("Content-Type", "Authorization") // 어떤 헤더들을 허용할 것인지
+//                .exposedHeaders("Content-Type", "Authorization")
+//                .allowCredentials(true) // 쿠키 요청을 허용한다(다른 도메인 서버에 인증하는 경우에만 사용해야하며, true 설정시 보안상 이슈가 발생할 수 있다)
+//                .maxAge(216000); // preflight 요청에 대한 응답을 브라우저에서 캐싱하는 시간;
+//    }
 
 }
